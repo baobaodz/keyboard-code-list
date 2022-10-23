5a22af49f973b387731eb3b7d410f3b141fe9f83
baobaodz <3208417157@qq.com>
2022-10-23 23:35:57 +0800

    update: 🚚抽取data

---
+++ data.js
@@ -0,0 +1,589 @@
+const keyData = [{
+    key: 'A',
+    code: 'keyA',
+    keyCode: 65,
+}, {
+    key: 'B',
+    code: 'keyB',
+    keyCode: 66,
+}, {
+    key: 'C',
+    code: 'keyC',
+    keyCode: 67,
+}, {
+    key: 'D',
+    code: 'keyD',
+    keyCode: 68,
+}, {
+    key: 'E',
+    code: 'keyE',
+    keyCode: 69,
+}, {
+    key: 'F',
+    code: 'keyF',
+    keyCode: 70,
+}, {
+    key: 'G',
+    code: 'keyG',
+    keyCode: 71,
+}, {
+    key: 'H',
+    code: 'keyH',
+    keyCode: 72,
+}, {
+    key: 'I',
+    code: 'keyI',
+    keyCode: 73,
+}, {
+    key: 'J',
+    code: 'keyJ',
+    keyCode: 74,
+}, {
+    key: 'K',
+    code: 'keyK',
+    keyCode: 75,
+}, {
+    key: 'L',
+    code: 'keyL',
+    keyCode: 76,
+}, {
+    key: 'M',
+    code: 'keyM',
+    keyCode: 77,
+}, {
+    key: 'N',
+    code: 'keyN',
+    keyCode: 78,
+}, {
+    key: 'O',
+    code: 'keyO',
+    keyCode: 79,
+}, {
+    key: 'P',
+    code: 'keyP',
+    keyCode: 80,
+}, {
+    key: 'Q',
+    code: 'keyQ',
+    keyCode: 81,
+}, {
+    key: 'R',
+    code: 'keyA',
+    keyCode: 82,
+}, {
+    key: 'S',
+    code: 'keyS',
+    keyCode: 83,
+}, {
+    key: 'T',
+    code: 'keyT',
+    keyCode: 84,
+}, {
+    key: 'U',
+    code: 'keyU',
+    keyCode: 85,
+}, {
+    key: 'V',
+    code: 'keyV',
+    keyCode: 86,
+}, {
+    key: 'W',
+    code: 'keyW',
+    keyCode: 87,
+}, {
+    key: 'X',
+    code: 'keyX',
+    keyCode: 88,
+}, {
+    key: 'Y',
+    code: 'keyY',
+    keyCode: 89,
+}, {
+    key: 'Z',
+    code: 'keyZ',
+    keyCode: 90,
+}, {
+    key: 'a',
+    code: 'keyA',
+    keyCode: 65,
+}, {
+    key: 'b',
+    code: 'keyB',
+    keyCode: 66,
+}, {
+    key: 'c',
+    code: 'keyC',
+    keyCode: 67,
+}, {
+    key: 'd',
+    code: 'keyD',
+    keyCode: 68,
+}, {
+    key: 'e',
+    code: 'keyE',
+    keyCode: 69,
+}, {
+    key: 'f',
+    code: 'keyF',
+    keyCode: 70,
+}, {
+    key: 'g',
+    code: 'keyG',
+    keyCode: 71,
+}, {
+    key: 'h',
+    code: 'keyH',
+    keyCode: 72,
+}, {
+    key: 'i',
+    code: 'keyI',
+    keyCode: 73,
+}, {
+    key: 'j',
+    code: 'keyJ',
+    keyCode: 74,
+}, {
+    key: 'k',
+    code: 'keyK',
+    keyCode: 75,
+}, {
+    key: 'l',
+    code: 'keyL',
+    keyCode: 76,
+}, {
+    key: 'm',
+    code: 'keyM',
+    keyCode: 77,
+}, {
+    key: 'n',
+    code: 'keyN',
+    keyCode: 78,
+}, {
+    key: 'o',
+    code: 'keyO',
+    keyCode: 79,
+}, {
+    key: 'p',
+    code: 'keyP',
+    keyCode: 80,
+}, {
+    key: 'q',
+    code: 'keyQ',
+    keyCode: 81,
+}, {
+    key: 'r',
+    code: 'keyA',
+    keyCode: 82,
+}, {
+    key: 's',
+    code: 'keyS',
+    keyCode: 83,
+}, {
+    key: 't',
+    code: 'keyT',
+    keyCode: 84,
+}, {
+    key: 'u',
+    code: 'keyU',
+    keyCode: 85,
+}, {
+    key: 'v',
+    code: 'keyV',
+    keyCode: 86,
+}, {
+    key: 'w',
+    code: 'keyW',
+    keyCode: 87,
+}, {
+    key: 'x',
+    code: 'keyX',
+    keyCode: 88,
+}, {
+    key: 'y',
+    code: 'keyY',
+    keyCode: 89,
+}, {
+    key: 'z',
+    code: 'keyZ',
+    keyCode: 90,
+}, {
+    key: '0',
+    code: 'Digit0',
+    keyCode: 48,
+}, {
+    key: '1',
+    code: 'Digit1',
+    keyCode: 49,
+}, {
+    key: '2',
+    code: 'Digit2',
+    keyCode: 50,
+}, {
+    key: '3',
+    code: 'Digit3',
+    keyCode: 51,
+}, {
+    key: '4',
+    code: 'Digit4',
+    keyCode: 52,
+}, {
+    key: '5',
+    code: 'Digit5',
+    keyCode: 53,
+}, {
+    key: '6',
+    code: 'Digit6',
+    keyCode: 54,
+}, {
+    key: '7',
+    code: 'Digit7',
+    keyCode: 55,
+}, {
+    key: '8',
+    code: 'Digit8',
+    keyCode: 56,
+}, {
+    key: '9',
+    code: 'Digit9',
+    keyCode: 57,
+}, {
+    key: '0',
+    code: 'Numpad0',
+    keyCode: 96,
+}, {
+    key: '1',
+    code: 'Numpad1',
+    keyCode: 97,
+}, {
+    key: '2',
+    code: 'Numpad2',
+    keyCode: 98,
+}, {
+    key: '3',
+    code: 'Numpad3',
+    keyCode: 99,
+}, {
+    key: '4',
+    code: 'Numpad4',
+    keyCode: 100,
+}, {
+    key: '5',
+    code: 'Numpad5',
+    keyCode: 101,
+}, {
+    key: '6',
+    code: 'Numpad6',
+    keyCode: 102,
+}, {
+    key: '7',
+    code: 'Numpad7',
+    keyCode: 103,
+}, {
+    key: '8',
+    code: 'Numpad8',
+    keyCode: 104,
+}, {
+    key: '9',
+    code: 'Numpad9',
+    keyCode: 105,
+}, {
+    key: '*',
+    code: 'NumpadMultiply',
+    keyCode: 106,
+}, {
+    key: '+',
+    code: 'NumpadAdd',
+    keyCode: 107,
+}, {
+    key: 'Enter',
+    code: 'Numpad9',
+    keyCode: 108,
+}, {
+    key: '-',
+    code: 'NumpadSubtract',
+    keyCode: 109,
+}, {
+    key: '.',
+    code: 'NumpadDecimal',
+    keyCode: 110,
+}, {
+    key: '/',
+    code: 'NumpadDivide',
+    keyCode: 111,
+}, {
+    key: 'F1',
+    code: 'F1',
+    keyCode: 112,
+}, {
+    key: 'F2',
+    code: 'F2',
+    keyCode: 113,
+}, {
+    key: 'F3',
+    code: 'F3',
+    keyCode: 114,
+}, {
+    key: 'F4',
+    code: 'F4',
+    keyCode: 115,
+}, {
+    key: 'F5',
+    code: 'F5',
+    keyCode: 116,
+}, {
+    key: 'F6',
+    code: 'F6',
+    keyCode: 117,
+}, {
+    key: 'F7',
+    code: 'F7',
+    keyCode: 118,
+}, {
+    key: 'F8',
+    code: 'F8',
+    keyCode: 119,
+}, {
+    key: 'F9',
+    code: 'F9',
+    keyCode: 120,
+}, {
+    key: 'F10',
+    code: 'F10',
+    keyCode: 121,
+}, {
+    key: 'F11',
+    code: 'F11',
+    keyCode: 122,
+}, {
+    key: 'F12',
+    code: 'F12',
+    keyCode: 123,
+}, {
+    key: 'BackSpace',
+    code: 'Backspace',
+    keyCode: 8,
+}, {
+    key: 'Tab',
+    code: 'Tab',
+    keyCode: 9,
+}, {
+    key: 'Clear',
+    code: 'Numpad5',
+    keyCode: 12,
+}, {
+    key: 'Enter',
+    code: 'Enter',
+    keyCode: 13,
+}, {
+    key: 'Enter',
+    code: 'NumpadEnter',
+    keyCode: 13,
+}, {
+    key: 'Shift',
+    code: 'ShiftLeft',
+    keyCode: 16,
+}, {
+    key: 'Shift',
+    code: 'ShiftRight',
+    keyCode: 16,
+}, {
+    key: 'Control',
+    code: 'ControlLeft',
+    keyCode: 17,
+}, {
+    key: 'Control',
+    code: 'ControlRight',
+    keyCode: 17,
+}, {
+    key: 'Alt',
+    code: 'AltLeft',
+    keyCode: 18,
+}, {
+    key: 'Alt',
+    code: 'AltRight',
+    keyCode: 18,
+}, {
+    key: 'CapsLock',
+    code: 'CapsLock',
+    keyCode: 20,
+}, {
+    key: 'Escape',
+    code: 'Escape',
+    keyCode: 27,
+}, {
+    key: ' ',
+    code: 'Space',
+    keyCode: 32,
+}, {
+    key: 'PageUp',
+    code: 'Numpad9',
+    keyCode: 33,
+}, {
+    key: 'PageDown',
+    code: 'Numpad3',
+    keyCode: 34,
+}, {
+    key: 'End',
+    code: 'Numpad1',
+    keyCode: 35,
+}, {
+    key: 'Home',
+    code: 'Numpad7',
+    keyCode: 36,
+}, {
+    key: 'ArrowLeft',
+    code: 'ArrowLeft',
+    keyCode: 37,
+}, {
+    key: 'ArrowUp',
+    code: 'ArrowUp',
+    keyCode: 38,
+}, {
+    key: 'ArrowRight',
+    code: 'ArrowRight',
+    keyCode: 39,
+}, {
+    key: 'ArrowDown',
+    code: 'ArrowDown',
+    keyCode: 40,
+}, {
+    key: 'Insert',
+    code: 'Insert',
+    keyCode: 45,
+}, {
+    key: 'Delete',
+    code: 'Delete',
+    keyCode: 46,
+}, {
+    key: 'NumLock',
+    code: 'NumLock',
+    keyCode: 144,
+}, {
+    key: ';',
+    code: 'Semicolon',
+    keyCode: 186,
+}, {
+    key: ':',
+    code: 'Semicolon',
+    keyCode: 186,
+}, {
+    key: '+',
+    code: 'Equal',
+    keyCode: 187,
+}, {
+    key: '=',
+    code: 'Equal',
+    keyCode: 187,
+}, {
+    key: ',',
+    code: 'Comma',
+    keyCode: 188,
+}, {
+    key: '<',
+    code: 'Comma',
+    keyCode: 188,
+}, {
+    key: '-',
+    code: 'Minus',
+    keyCode: 189,
+}, {
+    key: '_',
+    code: 'Minus',
+    keyCode: 189,
+}, {
+    key: '.',
+    code: 'Period',
+    keyCode: 190,
+}, {
+    key: '>',
+    code: 'Period',
+    keyCode: 190,
+}, {
+    key: '/',
+    code: 'Slash',
+    keyCode: 191,
+}, {
+    key: '?',
+    code: 'Slash',
+    keyCode: 191,
+}, {
+    key: '`',
+    code: 'Backquote',
+    keyCode: 192,
+}, {
+    key: '~',
+    code: 'Backquote',
+    keyCode: 192,
+}, {
+    key: '[',
+    code: 'BracketLeft',
+    keyCode: 219,
+}, {
+    key: '{',
+    code: 'BracketLeft',
+    keyCode: 219,
+}, {
+    key: '\\',
+    code: 'Backslash',
+    keyCode: 220,
+}, {
+    key: '|',
+    code: 'Backslash',
+    keyCode: 220,
+}, {
+    key: ']',
+    code: 'BracketRight',
+    keyCode: 221,
+}, {
+    key: '}',
+    code: 'BracketRight',
+    keyCode: 221,
+}, {
+    key: '"',
+    code: 'Quote',
+    keyCode: 222,
+}, {
+    key: '\'',
+    code: 'Quote',
+    keyCode: 222,
+}, {
+    key: 'AudioVolumeMute',
+    code: '',
+    keyCode: 173,
+}, {
+    key: 'AudioVolumeDown',
+    code: '',
+    keyCode: 174,
+}, {
+    key: 'AudioVolumeUp',
+    code: '',
+    keyCode: 175,
+}, {
+    key: 'MediaTrackNext',
+    code: '',
+    keyCode: 176,
+}, {
+    key: 'MediaTrackPrevious',
+    code: '',
+    keyCode: 177,
+}, {
+    key: 'MediaPlayPause',
+    code: '',
+    keyCode: 179,
+}, {
+    key: 'LaunchMediaPlayer',
+    code: '',
+    keyCode: 181,
+}, {
+    key: 'LaunchApplication1',
+    code: '',
+    keyCode: 182,
+}, {
+    key: 'LaunchApplication2', // 计算器
+    code: '',
+    keyCode: 183,
+}, {
+    key: 'LaunchApplication2',
+    code: '',
+    keyCode: 183,
+}]
